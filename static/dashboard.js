379b3ef71e946133de7727fc05b990b792befe61
dais-heroique <130305493+dais-heroique@users.noreply.github.com>
2026-08-26 20:21:10 +0200

    refactor: load lightweight console shell

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,3 +1,4 @@
 document.write('<script src="/static/dashboard-core.js"><\/script>');
 document.write('<script src="/static/conforva-brand.js"><\/script>');
+document.write('<script src="/static/lean-console.js"><\/script>');
 document.write('<link rel="stylesheet" href="/static/lean-console.css">');
