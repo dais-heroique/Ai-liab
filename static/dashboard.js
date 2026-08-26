aa0f4ae0b341d9b6b415f16e59b3f6ba49d4c9c6
dais-heroique <130305493+dais-heroique@users.noreply.github.com>
2026-08-26 13:39:59 +0200

    feat: load Conforva premium control-plane experience

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,3 +1,4 @@
 document.write('<script src="/static/dashboard-core.js"><\/script>');
 document.write('<script src="/static/enterprise-ux.js"><\/script>');
 document.write('<script src="/static/conforva-brand.js"><\/script>');
+document.write('<script src="/static/conforva-premium.js"><\/script>');
